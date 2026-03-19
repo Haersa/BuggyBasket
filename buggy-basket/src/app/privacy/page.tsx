a3a4233cb707bd133627953a1a0725e6daf78093
Harris <Harris@Haersa.com>
2026-03-19 15:32:25 +0000

    new pages

---
+++ buggy-basket/src/app/privacy/page.tsx
@@ -0,0 +1,10 @@
+export default function Privacy() {
+    return (
+      <div className="page">
+        <div className="page-container">
+          <h1>Privacy Policy</h1>
+          <p>Content goes here.</p>
+        </div>
+      </div>
+    )
+  }
---
+++ buggy-basket/src/app/terms/page.tsx
@@ -0,0 +1,10 @@
+export default function Terms() {
+    return (
+      <div className="page">
+        <div className="page-container">
+          <h1>Terms and Conditions</h1>
+          <p>Content goes here.</p>
+        </div>
+      </div>
+    )
+  }
