0d7d779336a93b7f045868d92b860157cfeb8b42
Harris <Harris@Haersa.com>
2026-03-19 16:00:32 +0000

    new page

---
+++ buggy-basket/src/app/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+export default function PasswordRecovery() {
+    return (
+      <div className="page">
+        <div className="page-container">
+          <h1>Password Recovery</h1>
+          <p>Content goes here.</p>
+        </div>
+      </div>
+    )
+  }
