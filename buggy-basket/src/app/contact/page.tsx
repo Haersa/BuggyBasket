872a3986422030420f9dd65452db4e788d86f770
40646580 <40646580@live.napier.ac.uk>
2026-03-23 18:08:42 +0000

    First draft of contact page. Changes will be made

--- buggy-basket/src/app/contact/page.tsx
+++ buggy-basket/src/app/contact/page.tsx
@@ -1,10 +1,39 @@
 export default function Contact() {
-    return (
-      <div className="page">
-        <div className="page-container">
-          <h1>Contact</h1>
-          <p>Content goes here.</p>
+  return (
+    <div className="page">
+      <div className="contact-container">
+        <h1 className="contact-title">Contact Us</h1>
+        <div className="contact-section">
+
+          <div className="contact-map">
+            <iframe src="https://www.google.com/maps?q=Thurso&output=embed"
+              width="100%" height="100%"
+              style={{ border: 0, borderRadius: "12px" }}
+              loading="lazy"></iframe>
+          </div>
+
+          <div className="contact-page">
+            <div className="contact-info">
+              <h2>Get in touch</h2>
+              <br />
+              <p>Email: Contact@buggybasket.co.uk</p>
+              <br />
+              <p>Phone: (Phone number here)</p>
+              <br />
+              <p>Location: Thurso, Scotland, UK</p>
+            </div>
+
+            <form className="contact-form">
+              <input type="text" placeholder="Your Name" />
+              <input type="text" placeholder="Your Email" />
+              <textarea placeholder="Your Message"></textarea>
+              <button type="submit">Send Message</button>
+            </form>
+
+          </div>
+
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
