c1bde60b7567f8b5fb3b5c6ac57d8c655bb99964
40646580 <40646580@live.napier.ac.uk>
2026-03-23 14:04:48 +0000

    Layout for about us page

--- buggy-basket/src/app/about/page.tsx
+++ buggy-basket/src/app/about/page.tsx
@@ -1,10 +1,45 @@
 export default function About() {
-    return (
-      <div className="page">
-        <div className="page-container">
-          <h1>About Us</h1>
-          <p>Content goes here.</p>
+  return (
+    <div className="page">
+      <div className="about-container">
+        <h1 className="about-title">About Us</h1>
+
+        <div className="about-section">
+          <div className="about-text">
+            <h2>Who we are</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit accusantium officia hic perferendis, quaerat vitae mollitia
+              perspiciatis porro voluptatibus, repellendus dolorem ad quasi facilis sint, molestias eum inventore consequatur voluptatum.</p>
+          </div>
+          <div className="about-image">
+            <div className="image-placeholder">Image</div>
+          </div>
         </div>
+
+        <div className="about-section reverse">
+          <div className="about-text">
+            <h2>What we do</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea eum, vero architecto
+              error recusandae itaque alias aspernatur magnam! At sint perferendis non voluptatem beatae id fugiat,
+              nisi cumque ipsam.</p>
+          </div>
+          <div className="about-image">
+            <div className="image-placeholder">Image</div>
+          </div>
+        </div>
+
+        <div className="about-section">
+          <div className="about-text">
+            <h2>Our Story</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro itaque veniam doloremque aperiam vero officiis adipisci
+              tenetur laudantium a? Nemo eos similique laudantium animi nobis quia expedita architecto odit deleniti, doloribus autem
+              perspiciatis enim pariatur molestias tempore quaerat at saepe sed, libero reprehenderit ducimus id.</p>
+          </div>
+          <div className="about-image">
+            <div className="image-placeholder">Image</div>
+          </div>
+        </div>
+
       </div>
-    )
-  }
+    </div>
+  )
+}
