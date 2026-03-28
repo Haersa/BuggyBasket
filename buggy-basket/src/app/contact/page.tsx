2c4ea206587167562ef46d7c3d333a2b9c037d85
40646580 <40646580@live.napier.ac.uk>
2026-03-28 15:04:46 +0000

    MOved map back to left

--- buggy-basket/src/app/contact/page.tsx
+++ buggy-basket/src/app/contact/page.tsx
@@ -5,6 +5,29 @@ export default function Contact() {
         <h1 className="contact-title">Contact Us</h1>
         <div className="contact-section">
 
+          <div className="contact-map">
+            <iframe src="https://www.google.com/maps?q=Thurso&output=embed"
+              width="100%" height="100%"
+              style={{ border: 0, borderRadius: "12px" }}
+              loading="lazy"></iframe>
+          </div>
+
+          <form className="contact-form">
+            <h2>Have an enquiry?</h2>
+            <div className="modal-field">
+              <label className="modal-label">Name</label>
+              <input type="text" className="modal-input" />
+            </div>
+            <div className="modal-field">
+              <label className="modal-label">Email</label>
+              <input type="text" className="modal-input" />
+            </div>
+            <div className="modal-field">
+              <label className="modal-label">Message</label>
+              <input type="text" className="modal-input" />
+            </div>
+            <button className="modal-submit">Submit</button>
+          </form>
           <div className="contact-page">
             <div className="contact-info">
               <h2>Get in touch</h2>
@@ -15,30 +38,8 @@ export default function Contact() {
               <br />
               <p>Location: Thurso, Scotland, UK</p>
             </div>
+          </div>
 
-            <form className="contact-form">
-              <h2>Have an enquiry?</h2>
-              <div className="modal-field">
-                <label className="modal-label">Name</label>
-                <input type="text" className="modal-input" />
-              </div>
-              <div className="modal-field">
-                <label className="modal-label">Email</label>
-                <input type="text" className="modal-input" />
-              </div>
-              <div className="modal-field">
-                <label className="modal-label">Message</label>
-                <input type="text" className="modal-input" />
-              </div>
-              <button className="modal-submit">Submit</button>
-            </form>
-          </div>
-          <div className="contact-map">
-            <iframe src="https://www.google.com/maps?q=Thurso&output=embed"
-              width="100%" height="100%"
-              style={{ border: 0, borderRadius: "12px" }}
-              loading="lazy"></iframe>
-          </div>
 
         </div>
       </div>
