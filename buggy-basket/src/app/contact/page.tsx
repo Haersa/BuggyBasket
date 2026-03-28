9e258ffbf1344888a5e4bb89328de0a3f6a0953e
40646580 <40646580@live.napier.ac.uk>
2026-03-28 15:45:00 +0000

    Adjusting the form and map on the contact page

--- buggy-basket/src/app/contact/page.tsx
+++ buggy-basket/src/app/contact/page.tsx
@@ -1,10 +1,10 @@
+import { Mail, Phone, MapPin, MessageCircle } from "lucide-react";
 export default function Contact() {
   return (
     <div className="page">
       <div className="contact-container">
         <h1 className="contact-title">Contact Us</h1>
         <div className="contact-section">
-
           <div className="contact-map">
             <iframe src="https://www.google.com/maps?q=Thurso&output=embed"
               width="100%" height="100%"
@@ -12,37 +12,63 @@ export default function Contact() {
               loading="lazy"></iframe>
           </div>
 
-          <form className="contact-form">
-            <h2>Have an enquiry?</h2>
-            <div className="modal-field">
-              <label className="modal-label">Name</label>
-              <input type="text" className="modal-input" />
-            </div>
-            <div className="modal-field">
-              <label className="modal-label">Email</label>
-              <input type="text" className="modal-input" />
-            </div>
-            <div className="modal-field">
-              <label className="modal-label">Message</label>
-              <input type="text" className="modal-input" />
-            </div>
-            <button className="modal-submit">Submit</button>
-          </form>
           <div className="contact-page">
-            <div className="contact-info">
-              <h2>Get in touch</h2>
-              <br />
-              <p>Email: Contact@buggybasket.co.uk</p>
-              <br />
-              <p>Phone: (Phone number here)</p>
-              <br />
-              <p>Location: Thurso, Scotland, UK</p>
+            <form className="contact-form">
+              <p className="modal-text">Send a message</p>
+              <h2>Have an enquiry?</h2>
+              <div className="modal-field">
+                <label className="modal-label">Firstname</label>
+                <input type="text" className="modal-input" />
+              </div>
+              <div className="modal-field">
+                <label className="modal-label">Surname</label>
+                <input type="text" className="modal-input" />
+              </div>
+              <div className="modal-field">
+                <label className="modal-label">Email</label>
+                <input type="text" className="modal-input" />
+              </div>
+              <div className="modal-field">
+                <label className="modal-label">Subject</label>
+                <select className="modal-input">
+                  <option value="Pricing">Pricing</option>
+                  <option value="Delivery">Delivery</option>
+                  <option value="General">General Question</option>
+                </select>
+              </div>
+              <div className="modal-field">
+                <label className="modal-label">Message</label>
+                <input type="text" className="modal-input" />
+              </div>
+              <button className="modal-submit">Submit</button>
+            </form>
+            <div className="contact-details-grid">
+
+              <div className="contact-card">
+                <Mail className="contact-icon"></Mail>
+                <p>-add email here-</p>
+              </div>
+
+              <div className="contact-card">
+                <Phone className="contact-icon"></Phone>
+                <p>-add phone here-</p>
+              </div>
+
+              <div className="contact-card">
+                <MapPin className="contact-icon"></MapPin>
+                <p>Thurso, UK</p>
+              </div>
+
+              <div className="contact-card">
+                <MessageCircle className="contact-icon"></MessageCircle>
+                <p>Coming soon...</p>
+              </div>
             </div>
+
+
           </div>
-
-
         </div>
       </div>
-    </div>
+    </div >
   )
 }
