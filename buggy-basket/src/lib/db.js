245a529430a2aec41845cda513fdf4b3b2eeffba
Harris <Harris@Haersa.com>
2026-05-04 20:20:12 +0100

    small bug fix

--- buggy-basket/src/lib/db.js
+++ buggy-basket/src/lib/db.js
@@ -1,9 +1,9 @@
-const Database = require("better-sqlite3");
-const path = require("path");
+const Database = require('better-sqlite3');
+const path = require('path');
 
-const dbPath = path.join(__dirname, "..", "..", "database.db");
+const dbPath = path.join(process.cwd(), 'database.db');
 const db = new Database(dbPath);
 
-db.pragma("journal_mode = WAL");
+db.pragma('journal_mode = WAL');
 
 module.exports = db;
