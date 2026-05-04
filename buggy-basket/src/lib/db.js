e5dd6e019fb3875d3d23e92a62beed07820fb812
Harris <haersa@gmail.com>
2026-05-04 16:33:17 +0100

    patch

--- buggy-basket/src/lib/db.js
+++ buggy-basket/src/lib/db.js
@@ -1,10 +1,9 @@
-const Database = require('better-sqlite3');
-const path = require('path');
+const Database = require("better-sqlite3");
+const path = require("path");
 
-const dbPath = path.join(process.cwd(), 'database.db');
+const dbPath = path.join(__dirname, "..", "..", "database.db");
 const db = new Database(dbPath);
 
-// enable WAL mode for better performance
-db.pragma('journal_mode = WAL');
+db.pragma("journal_mode = WAL");
 
 module.exports = db;
